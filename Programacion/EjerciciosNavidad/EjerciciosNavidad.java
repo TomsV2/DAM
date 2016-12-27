f43292a3f01076f247968e8fdcfda2426ab00785
Tom_s <tomasmateo98@gmail.com>
2016-12-28 00:31:30 +0100

    EjerciciosNavidad
    Principio del menú para la posterior implementación de subprogramas.

---
+++ Programacion/EjerciciosNavidad/EjerciciosNavidad.java
@@ -0,0 +1,103 @@
+/*
+ * Nombre: EjerciciosNavidad
+ * 
+ * Comentario: Colección de los ejecicios de navidad
+ * 
+ * Entradas: Opción de menú
+ * Salidas: Ninguna
+ * Restri: La opción será una de las mostradas en el manú
+ * 
+ * PG:
+ * 
+ *	//Inicio
+ *		//Repetir
+ *			//Mostrar menú, leer y validar opción
+ *			//Si opcion >= 1 y opcion <=4
+ *				//Según opción
+ *					//caso 1:
+ *						//Sumar
+ *					//caso 2:
+ *						//Restar
+ *					//caso 3:
+ *						//Multiplicar
+ *	 				//caso 4:
+ *						//Dividir
+ *				//Fin_Según
+ *				//Preguntar para repetir
+ *			//Fin_Si
+ *		//Mientras se quiera repetir y opcion !=0
+ *	//Fin
+ *
+ * 
+ */
+
+import java.util.*;
+
+public class EjerciciosNavidad {
+	
+	public static void main (String[] args) {
+		
+		Scanner teclado = new Scanner (System.in);
+		
+		int opcion=0;
+		char respuesta=' ';
+		
+//Inicio
+	//Repetir
+	do{
+		//Mostrar menú, leer y validar opción
+		//Si opcion >= 1 y opcion <=4
+			//Según opción
+				//caso 1:
+					//Ejercicio1
+			//Fin_Según
+			//Preguntar para repetir
+		//Fin_Si
+	}
+	//Mientras se quiera repetir y opcion !=0
+	while (respuesta=='s' && opcion !=0);
+//Fin
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+		
+		/*//Inicio
+			//Repetir
+			do{
+				//mostrarMenu
+				System.out.println("");
+				System.out.println("Bienvenido a los ejercicios de Navidad");
+				System.out.println("");
+				System.out.println("Escoja una opcion del menu: ");
+				System.out.println("1 ---> Ejercicio1 - Caracteres del alfabeto");
+				
+				opcion = teclado.nextInt();
+				
+				if (opcion==1){
+					System.out.println("FUNCIONA :D");
+				}
+			}
+			//Mientras opcion sea valida y no sea 0
+			while (opcion!=1);
+				
+			
+		//Fin*/	
+	}
+}
+
