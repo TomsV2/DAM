eeee4887fcca8cfc7adf888dae2dae90a84b3215
Tom_s <tomasmateo98@gmail.com>
2017-01-10 10:14:19 +0100

    Boletin7.2
    Boletin de ejercicios de base de datos, consultas.

---
+++ BD/Boletin7.2/Boletin7punto2.sql
@@ -0,0 +1,13 @@
+USE Bichos
+GO
+
+DROP DATABASE Boletin7punto2
+GO
+
+CREATE DATABASE Boletin7punto2
+GO
+
+USE Boletin7punto2
+GO
+
+--1. Título, precio y notas de los libros (titles) que tratan de cocina, ordenados de mayor a menor precio.
