9c73999161a2ddb4c780957ffcb9033476a7c458
Tom_s <tomasmateo98@gmail.com>
2017-01-20 11:01:09 +0100

    Boletín 7.3 - BD
    Casi todos los ejercicios terminados.

--- BD/Boletin7.3/Boletin7punto3.sql
+++ BD/Boletin7.3/Boletin7punto3.sql
@@ -4,4 +4,13 @@ SELECT * FROM Production.Product
 SELECT Name, ProductNumber, ListPrice, Color FROM Production.Product WHERE (Color in ('Red', 'Yellow')) AND (ListPrice BETWEEN 50 AND 500)
 
 --2. Nombre, número de producto, precio de coste,  precio de venta, margen de beneficios total y margen de beneficios en % del precio de venta de los productos de categorías inferiores a 16.
-SELECT Name, ProductNumber, StandardCost, ListPrice, (ListPrice - StandardCost) AS Beneficios, (ListPrice - StandardCost*100)  FROM Production.Product WHERE 
+SELECT Name, ProductNumber, StandardCost, ListPrice, (ListPrice - StandardCost) AS Beneficios, ((ListPrice - StandardCost)/(StandardCost/100)) AS [Beneficios(%)]  FROM Production.Product WHERE ProductModelId < 16
+
+--3. Empleados cuyo nombre o apellidos contenga la letra "r". Los empleados son los que tienen el valor "EM" en la columna PersonType de la tabla Person.
+SELECT FirstName, LastName, PersonType FROM Person.Person WHERE LastName like '%r%' AND PersonType = 'EM'
+
+--4. LoginID, nationalIDNumber, edad y puesto de trabajo (jobTitle) de los empleados (tabla Employees) de sexo femenino que tengan más de cinco años de antigüedad.
+SELECT * FROM HumanResources.Employee
+
+SELECT LoginID, NationalIDNumber, ((YEAR(CURRENT_TIMESTAMP-CAST(BirthDate AS DATETIME))-1900)) AS Edad, JobTitle FROM HumanResources.Employee
+
