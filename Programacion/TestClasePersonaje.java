c787ce374c57b89b274fb21af55f4e2f8d566a6e
Tom_s <tomasmateo98@gmail.com>
2017-02-08 11:59:57 +0100

    Test de la clase Personaje

--- Programacion/TestClasePersonaje.java
+++ Programacion/TestClasePersonaje.java
@@ -7,6 +7,16 @@ public class TestClasePersonaje {
 		System.out.println("El nombre por defecto es: " +personaje1.getNombre());
 		System.out.println(" ");
 		System.out.println("Probando el toString" +personaje1.toString());
+		
+		personaje1.setNombre("SrMieo");
+		
+		System.out.println(" ");
+		System.out.println("Probando el toString" +personaje1.toString());
+		
+		ClasePersonaje personaje2 = new ClasePersonaje("Tom_s", 5, 100, 0, 0, true);
+		
+		System.out.println(" ");
+		System.out.println("Probando el toString" +personaje2.toString());
 	}
 }
 
