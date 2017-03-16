07a9b9308a0ac93700c4cd1f7322e99a96a7a41e
Tom_s <tomasmateo98@gmail.com>
2017-03-16 23:44:35 +0100

    Juego de cartas

--- Programacion/ProgramaJuegoCartas/TestClaseCarta.java
+++ Programacion/ProgramaJuegoCartas/TestClaseCarta.java
@@ -9,6 +9,15 @@ public class TestClaseCarta {
 		ClaseCarta carta0 = new ClaseCarta();
 		
 		System.out.println(carta0.toString());
+		
+		System.out.println(" ");
+		System.out.println("--------------Constructor por defecto--------------");
+		System.out.println(" ");
+		
+		ClaseCarta carta1 = new ClaseCarta('o', 1);
+		
+		System.out.println(carta1.toString());
+		
 	}
 }
 
